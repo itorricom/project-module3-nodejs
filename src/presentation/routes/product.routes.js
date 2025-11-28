dfac12d75ba23447cbe09cec341b9e7bdb8bdd46
Isidoro Torrico <itorrico.m@gmail.com>
2025-11-27 20:26:42 -0400

    Implementar CRUD completo de productos con arquitectura limpia

--- src/presentation/routes/product.routes.js
+++ src/presentation/routes/product.routes.js
@@ -12,6 +12,9 @@ const productController = new ProductController(productService);
 
 const router = Router();
 router.get('/', productController.getAll);
+router.get('/:id', productController.getById);
 router.post('/', productController.create);
+router.put('/:id', productController.update);
+router.delete('/:id', productController.delete);
 
 module.exports = router;
